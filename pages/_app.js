0c377c7decde8c6b89146706e525596989587084
Anil <70024200+destinynokha@users.noreply.github.com>
2025-09-18 22:50:34 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,12 @@ export default function App({
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider 
+      session={session}
+      refetchInterval={5 * 60} // Refetch session every 5 minutes
+      refetchOnWindowFocus={false} // Don't refetch on window focus
+      refetchWhenOffline={false} // Don't refetch when offline
+    >
       <Component {...pageProps} />
     </SessionProvider>
   )
